c3c5357706fd63d64ca709c42d1b007eb15a6ea2
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-30 04:18:12 +1000

    built out register component

---
+++ better-reads-app/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Login(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Login.propTypes = {
+
+}
+
+export default Login
