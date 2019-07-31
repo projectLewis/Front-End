25987754afa64e7e07d6bbc68ecd26999363b418
Seth Lewis <lewiscatenary@gmail.com>
2019-07-31 18:02:24 -0500

    flex wrapper styles added

--- better-reads-app/src/components/RecommendedBooks.js
+++ better-reads-app/src/components/RecommendedBooks.js
@@ -3,10 +3,18 @@ import Book from "./Book";
 
 function RecommendedBooks({ recommendedBooks }) {
   console.log(recommendedBooks)
+
   return (
-    <>
+    <div className='bookFlexWrapper' style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      margin: '40px auto',
+      maxWidth: '1000px',
+      padding: '0 20px',
+      justifyContent: 'center'
+    }}>
        {recommendedBooks.map(book=><Book book={book}/>)} 
-    </>
+    </div>
   );
 }
 
