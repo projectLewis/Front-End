8771c324105a01e012425e0f8744b9e12ef301f6
SierraOG <sierrao@utexas.edu>
2019-07-31 10:05:03 -0500

    modal functionality work

--- better-reads-app/src/components/BookModal.js
+++ better-reads-app/src/components/BookModal.js
@@ -4,7 +4,7 @@ import { Button, Header, Image, Modal } from 'semantic-ui-react';
 import Axios from "axios";
 
 
-const BookModal = (props) => {
+const BookModal = ({isbn}) => {
     // const [isbn, setIsbn] = useState(0)
     // useEffect(() => {
     //     if (isbn) {
@@ -12,17 +12,7 @@ const BookModal = (props) => {
     //    }
     // }, [input])
     return (
-        <Modal open>
-        <Modal.Header>Select a Photo</Modal.Header>
-        <Modal.Content image>
-        <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
-        <Modal.Description>
-        <Header>Default Profile Image</Header>
-            <p>We've found the following gravatar image associated with your e-mail address.</p>
-            <p>Is it okay to use this photo?</p>
-            </Modal.Description>
-        </Modal.Content>
-        </Modal>
+        <div>{isbn}</div>
     )
 }
 
