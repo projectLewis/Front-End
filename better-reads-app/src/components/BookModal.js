d6792f21e9a284da34ac43febbee55c8e0a0154f
SierraOG <sierrao@utexas.edu>
2019-07-31 10:41:51 -0500

    added loading animation and more info to modal

--- better-reads-app/src/components/BookModal.js
+++ better-reads-app/src/components/BookModal.js
@@ -1,20 +1,32 @@
 import React,{useState, useEffect} from "react";
 import Axios from "axios";
-
+import { Card, Image, Dimmer, Loader } from 'semantic-ui-react'
 
 const BookModal = ({isbn}) => {
-    const [book, addBook] = useState()
+    const [book, addBook] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
         if (isbn) {
            Axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
                 .then(data=>{
-                    addBook(data.data.items[0].volumeInfo.description)
+                    addBook(data.data.items[0].volumeInfo)
+                    setIsLoading(false)
                 })
        }
     }, [isbn])
 
-    return (
-        <div>{book}</div>
+    return (isLoading) ? (
+        <Dimmer inverted active>
+          <Loader inverted > Loading </Loader>
+        </Dimmer>
+      )
+      : (
+        <>
+            <div>{book.description}</div>
+            <div>{book.pageCount} pages</div>
+            <div>Rating: {book.averageRating}/5</div>
+            <div>Maturity Rating: {book.maturityRating}</div>
+        </>
     )
 }
 
