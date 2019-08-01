ea3ddc0c6290167c6f04e484506d64597efe4fb7
Justine Gennaro <justine.rouzier@gmail.com>
2019-08-01 14:15:32 -0500

    nav modification 2:15pm

--- better-reads-app/src/App.js
+++ better-reads-app/src/App.js
@@ -7,9 +7,11 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Nav from "./components/Nav";
-import SavedBooks from "./components/SavedBooks"
+import SavedBooks from "./components/SavedBooks";
+import Footer from "./components/Footer";
 
 import "./App.css";
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <PrivateRoute exact path="/saved_books" component={SavedBooks} />
+      <Footer />
     </div>
   );
 }
