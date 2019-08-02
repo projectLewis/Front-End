b549756d671986edb8044cc0d0e525d1a57c0dea
Seth Lewis <lewiscatenary@gmail.com>
2019-08-02 00:41:41 -0500

    removed unused import

--- better-reads-app/src/components/Home.js
+++ better-reads-app/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Input, Form, Image, Dimmer, Loader } from "semantic-ui-react";
+import { Button, Input, Form, Dimmer, Loader } from "semantic-ui-react";
 
 import styled from "styled-components";
 
